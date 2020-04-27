acbc6ce17c27acf5453f94414f070f10cb7fc7a1
Сергей Юдин <enfros2000@gmail.com>
2020-04-27 08:56:54 +0300

    Initial commit

---
+++ src/serve-dev.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import SeeOnGithub from '@/see-on-github.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    SeeOnGithub
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <see-on-github linkTo="https://github.com/yudin-s/nuxt-ghpages-boilerplate" />
+  </div>
+</template>
